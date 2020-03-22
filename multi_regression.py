65d983c7d361c7385ba0ca47c3a2a7b340fd7ef9
Anand Namdev <anand.nitb19@gmail.com>
2020-03-22 23:01:33 +0530

    added code

--- multi_regression.py
+++ multi_regression.py
@@ -5,3 +5,19 @@ Created on Sun Mar 22 22:27:22 2020
 @author: annamdev
 """
 
+import pandas as pd
+
+trainFileName = "train.csv"
+testFileName = "test.csv"
+
+def GetFiles():
+    
+    train = pd.read_csv(trainFileName)
+    test = pd.read_csv(testFileName)
+    
+    return train, test
+    
+def ExploratoryDataAnalysis():
+    
+    # for each feature count nulls/NA/0
+    # check for outliers - remove them
